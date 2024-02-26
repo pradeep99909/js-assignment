f94811e2149b8c0563965d96addddf1d2ce8cfe1
Pradeep <pradeep99909@gmail.com>
2024-02-27 00:09:06 +0530

    express json added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helper = require("./src/lib/helper");
 const config = require('./config/config');
 const app = express();
+app.use(express.json())
 const port = config.server.port;
 
 //Register routes
